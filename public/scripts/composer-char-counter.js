70b5a9ea045903897cfe9d066f11b8c87fe87c5d
vagrant <kiel.strang@gmail.com>
2017-09-19 17:50:55 +0000

    Add character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+const MAX_LENGTH = 140;
+
+$(document).ready(function(){
+  $('.new-tweet textarea').on('keydown', function(){
+    const tweetLength = $(this).val().length;
+    const counter = $(this).siblings('.counter').first();
+    counter.text(MAX_LENGTH - tweetLength);
+
+    if(tweetLength > MAX_LENGTH) {
+      counter.addClass('red');
+    } else {
+      counter.removeClass('red');
+    }
+  });
+});
