4e01303dc804d1c82d00c8a5a45f7278fba96215
vagrant <kiel.strang@gmail.com>
2017-09-19 17:54:57 +0000

    Cleanup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 const MAX_LENGTH = 140;
 
-$(document).ready(function(){
-  $('.new-tweet textarea').on('keydown', function(){
+$(document).ready(function() {
+  $('.new-tweet textarea').on('keydown', function() {
     const tweetLength = $(this).val().length;
     const counter = $(this).siblings('.counter').first();
-    counter.text(MAX_LENGTH - tweetLength);
 
+    counter.text(MAX_LENGTH - tweetLength);
     if(tweetLength > MAX_LENGTH) {
-      counter.addClass('red');
+      counter.addClass('tweet-too-long');
     } else {
-      counter.removeClass('red');
+      counter.removeClass('tweet-too-long');
     }
   });
 });
