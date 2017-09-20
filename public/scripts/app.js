56cfbe57d154480976563de56d559c602db15357
vagrant <kiel.strang@gmail.com>
2017-09-20 18:03:54 +0000

    Move app.js helper functions into document-ready

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,67 +1,68 @@
-function createTweetElement(tweet) {
-  const $tweet = $('<article>').addClass('tweet')
-    .append(
-      $('<header>')
-        .append(
-          $('<div>').addClass('tweet-avatar')
-            .append($('<img>').attr('src', tweet.user.avatars.regular).attr('alt', tweet.user.handle))
-        )
-        .append(
-          $('<div>').addClass('tweet-user')
-            .append($('<span>').addClass('tweet-name').text(tweet.user.name))
-            .append($('<span>').addClass('tweet-handle').text(tweet.user.handle))
-        )
-    )
-    .append($('<div>').addClass('tweet-body').text(tweet.content.text))
-    .append(
-      $('<footer>')
-        .append(
-          $('<span>').addClass('tweet-time').text(moment(tweet.created_at).fromNow())
-        )
-        .append(
-          $('<div>').addClass('tweet-icons')
-            .append($('<img>').attr('src', '/images/icons/flag.svg').attr('alt', 'Flag'))
-            .append($('<img>').attr('src', '/images/icons/retweet.svg').attr('alt', 'Retweet'))
-            .append($('<img>').attr('src', '/images/icons/heart.svg').attr('alt', 'Heart'))
-        )
-    );
-  return $tweet;
-}
-
-function renderTweets(tweets) {
-  $('#tweets-container').empty();
-  for(tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('#tweets-container').prepend($tweet);
-  }
-}
-
-function loadTweets() {
-  $.ajax({
-    url: '/tweets',
-    method: 'GET',
-    success: function (response) {
-      renderTweets(response);
-    }
-  });
-}
-
-function submitTweet() {
-  $.ajax({
-    url: '/tweets',
-    method: 'POST',
-    data: {
-      text: $('.new-tweet textarea').val()
-    },
-    success: function() {
-      $('.new-tweet textarea').val('');
-      loadTweets();
-    }
-  });
-}
-
 
 $(function() {
+  function createTweetElement(tweet) {
+    const $tweet = $('<article>').addClass('tweet')
+      .append(
+        $('<header>')
+          .append(
+            $('<div>').addClass('tweet-avatar')
+              .append($('<img>').attr('src', tweet.user.avatars.regular).attr('alt', tweet.user.handle))
+          )
+          .append(
+            $('<div>').addClass('tweet-user')
+              .append($('<span>').addClass('tweet-name').text(tweet.user.name))
+              .append($('<span>').addClass('tweet-handle').text(tweet.user.handle))
+          )
+      )
+      .append($('<div>').addClass('tweet-body').text(tweet.content.text))
+      .append(
+        $('<footer>')
+          .append(
+            $('<span>').addClass('tweet-time').text(moment(tweet.created_at).fromNow())
+          )
+          .append(
+            $('<div>').addClass('tweet-icons')
+              .append($('<img>').attr('src', '/images/icons/flag.svg').attr('alt', 'Flag'))
+              .append($('<img>').attr('src', '/images/icons/retweet.svg').attr('alt', 'Retweet'))
+              .append($('<img>').attr('src', '/images/icons/heart.svg').attr('alt', 'Heart'))
+          )
+      );
+    return $tweet;
+  }
+
+  function renderTweets(tweets) {
+    $('#tweets-container').empty();
+    for(tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
+  }
+
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function (response) {
+        renderTweets(response);
+      }
+    });
+  }
+
+  function submitTweet() {
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: {
+        text: $('.new-tweet textarea').val()
+      },
+      success: function() {
+        $('.new-tweet textarea').val('');
+        loadTweets();
+      }
+    });
+  }
+
+
   loadTweets();
 
   $('.new-tweet input').on('click', function(event) {
