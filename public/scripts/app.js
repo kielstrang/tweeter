e2c0bbcab1e088de399c6cbaee6bfd95e8842785
vagrant <kiel.strang@gmail.com>
2017-09-20 00:59:31 +0000

    Change sample tweet to check for XSS vulnerabilities

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,120 +1,19 @@
-const past = 3;
-const now = new Date();
-
-const future = new Date().setDate(now.getDate() + 1);
-const secondsAgo = new Date().setSeconds(now.getSeconds() - past);
-const minutesAgo = new Date().setMinutes(now.getMinutes() - past);
-const hoursAgo = new Date().setHours(now.getHours() - past);
-const daysAgo = new Date().setDate(now.getDate() - past);
-const monthsAgo = new Date().setMonth(now.getMonth() - past);
-const yearsAgo = new Date().setFullYear(now.getFullYear() - past);
-
 const tweetData = {
   0: {
     "user": {
-      "name": "Newton",
+      "name": "<script>alert('username XSS!');</script>",
       "avatars": {
         "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
         "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
       },
-      "handle": "@SirIsaac"
+      "handle": "<script>alert('handle XSS!');</script>"
     },
     "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+      "text": "<script>alert('text XSS!');</script>"
     },
-    "created_at": future
-  },
-  1: {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": secondsAgo
-  },
-  2: {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": minutesAgo
-  },
-  3: {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": hoursAgo
-  },
-  4: {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": daysAgo
-  },
-  5: {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": monthsAgo
-  },
-  6: {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": yearsAgo
-  },
+    "created_at": new Date()
+  }
 };
 
 function createTweetElement(tweet) {
