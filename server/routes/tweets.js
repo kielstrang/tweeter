b612572be103887ebc3eab8b807ea2ea03970a1f
vagrant <kiel.strang@gmail.com>
2017-09-22 19:57:38 +0000

    Cleanup

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -9,54 +9,36 @@ module.exports = function(DataHelpers) {
 
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
-      if(err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.json(tweets);
-      }
+      if(err) return res.status(500).json({ error: err.message });
+
+      res.json(tweets);
     });
   });
 
   tweetsRoutes.post("/", function(req, res) {
-    if(!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body' });
-      return;
-    }
+    if(!req.body.text) return res.status(400).json({ error: 'invalid request: no data in POST body' });
 
     DataHelpers.validateLogin(req.body.handle, req.body.password, function(err, isValidLogin) {
-      if(err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
+      if(err) return res.status(500).json({ error: err.message });
+      if(!isValidLogin) return res.status(401).json({error: 'Invalid login'});
 
-      if(isValidLogin) {
-        DataHelpers.getUserDetails(req.body.handle, function(err, user) {
-          if(err) {
-            res.status(500).json({ error: err.message });
-            return;
-          }
+      DataHelpers.getUserDetails(req.body.handle, function(err, user) {
+        if(err) return res.status(500).json({ error: err.message });
 
-          const tweet = {
-            user: user,
-            content: {
-              text: req.body.text
-            },
-            created_at: Date.now()
-          };
+        const tweet = {
+          user: user,
+          content: {
+            text: req.body.text
+          },
+          created_at: Date.now()
+        };
 
-          DataHelpers.saveTweet(tweet, (err) => {
-            console.log(tweet);
-            if(err) {
-              res.status(500).json({ error: err.message });
-            } else {
-              res.status(201).send();
-            }
-          });
+        DataHelpers.saveTweet(tweet, (err) => {
+          if(err) return res.status(500).json({ error: err.message });
+
+          res.status(201).send();
         });
-      } else {
-        console.log(`User: ${req.body.handle} Password: ${req.body.password}`);
-        res.status(401).send();
-      }
+      });
     });
   });
 
