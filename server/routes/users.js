f5b513b0ed3a064cc5044d74774857fc3f8d94c9
vagrant <kiel.strang@gmail.com>
2017-09-22 23:47:41 +0000

    Set session cookie on register

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,40 +8,35 @@ const usersRoutes = express.Router();
 module.exports = function(DataHelpers) {
 
   usersRoutes.post("/login", function(req, res) {
-    const {handle, password} = req.body;
-    console.log(handle, password);
+    const { handle, password } = req.body;
     DataHelpers.validateLogin(handle, password, (err, isValidLogin) => {
       if(err) return res.status(500).json({ error: err.message });
       if(!isValidLogin) return res.status(401).json({error: "Invalid handle or password"});
 
       req.session.handle = handle;
-      return res.status(200).send();
+      res.status(200).send();
     });
   });
 
   usersRoutes.post("/new", function(req, res) {
-    console.log(req.body);
-    DataHelpers.checkHandleAvailable(req.body.handle, (err, isAvailable) => {
+    const { name, handle, password } = req.body;
+    DataHelpers.checkHandleAvailable(handle, (err, isAvailable) => {
       if(err) return res.status(500).json({ error: err.message });
-      if(!(req.body.handle && req.body.name && req.body.password)) {
+      if(!(handle && name && password)) {
         return res.status(400).json({error: "Missing registration field"});
       }
       if(!isAvailable) return res.status(400).json({error: "Handle already in use"});
 
-      const user = {
-        name: req.body.name,
-        handle: req.body.handle,
-        password: req.body.password,
-        avatars: userHelper.generateUserAvatars(req.body.handle)
-      };
+      const user = { name, handle, password };
+      user.avatars = userHelper.generateUserAvatars(handle);
       DataHelpers.createUser(user, (err) => {
         if(err) return res.status(500).json({ error: err.message });
 
+        req.session.handle = handle;
         res.status(201).send();
       });
     });
   });
 
   return usersRoutes;
-
 };
